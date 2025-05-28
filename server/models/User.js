18c92c3c481e382f4413371e77122ed1ca8ef591
AhmathNafees <ahamednafees15@gmail.com>
2025-05-28 16:58:07 +0530

    create Userjs in Nodejs

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{type:String, required:true},
+        email:{type:String, required:true},
+        Password:{type:String, required:true},
+        role:{type:String, enum:["admin","employee"], required:true},
+    }
+)
