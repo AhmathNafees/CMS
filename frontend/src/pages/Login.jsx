a66fb708bcbcc22750dc7dd8ef2ae157f83d04a2
AhmathNafees <ahamednafees15@gmail.com>
2025-05-29 16:57:30 +0530

    modify login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail]= useState('')
+    const [password, setPassword]= useState('')
+    const handelSubmit = async(e) =>{
+        e.preventDefault()
+        try{
+            const response = await axios.post("http://localhost:3000/api/auth/login")
+        }catch(error){
+            console.log(error)
+        }
+    }
   return (
     <div className='flex flex-col items-center h-screen justify-center bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6' >
         <h2 className='font-signika text-3xl text-white' >Customer Management System</h2>
         <div className=' border-0 shadow p-6 w-80 bg-white rounded'>
             <h2 className=' text-2xl font-bold mb-4'>Login</h2>
-            <form action="">
+            <form action="" onSubmit={handelSubmit}>
             
                 <div className=' mb-4' >
                     <label htmlFor="email" className=' block text-gray-700'>Email</label>
-                    <input type="email" placeholder='Enter Email' id='email' className=' w-full px-3 py-2 border rounded-md'/>
+                    <input type="email" placeholder='Enter Email' id='email' className=' w-full px-3 py-2 border rounded-md'
+                    onChange={(e)=> setEmail(e.target.value)}/>
                 </div>
                 <div className=' mb-4'>
                     <label htmlFor="password" className=' block text-gray-700'>Password</label>
-                    <input type="password" placeholder='*******' id='password' className=' w-full px-3 py-2 border rounded-md'/>
+                    <input type="password" placeholder='*******' id='password' className=' w-full px-3 py-2 border rounded-md'
+                    onChange={(e)=> setPassword(e.target.value)}/>
                 </div>
                 <div className=' mb-4 flex items-center justify-between'>
                     <label  className=' inline-flex items-center'>
