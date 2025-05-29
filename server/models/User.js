d0713f7aecf4af6688cd105b3dd1a80f1bb585b1
AhmathNafees <ahamednafees15@gmail.com>
2025-05-29 10:28:55 +0530

    created User js

--- server/models/User.js
+++ server/models/User.js
@@ -6,5 +6,11 @@ const userSchema = new mongoose.Schema(
         email:{type:String, required:true},
         Password:{type:String, required:true},
         role:{type:String, enum:["admin","employee"], required:true},
+        profileImage:{type:String},
+        createAt:{type: Date, default: Date.now},
+        updateAt:{type: Date, default : Date.now},
     }
 )
+
+const User = mongoose.model("User",userSchema)
+export default User
