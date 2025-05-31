2da8a873480a72dbe75fa38848cfef7fb156c29d
AhmathNafees <ahamednafees15@gmail.com>
2025-05-31 11:59:08 +0530

    Implement login page backend & Modify login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,12 +4,21 @@ import React, { useState } from 'react'
 const Login = () => {
     const [email, setEmail]= useState('')
     const [password, setPassword]= useState('')
+    const [error, setError] = useState(null)
+    
     const handelSubmit = async(e) =>{
         e.preventDefault()
         try{
-            const response = await axios.post("http://localhost:3000/api/auth/login")
+            const response = await axios.post("http://localhost:3000/api/auth/login",{email,password});
+            if(response.data.success){
+                alert("Successfully Login")
+            }
         }catch(error){
-            console.log(error)
+            if(error.response && !error.response.data.success){
+                setError(error.response.data.error)
+            }else{
+                setError("Server Error")
+            }
         }
     }
   return (
@@ -17,17 +26,20 @@ const Login = () => {
         <h2 className='font-signika text-3xl text-white' >Customer Management System</h2>
         <div className=' border-0 shadow p-6 w-80 bg-white rounded'>
             <h2 className=' text-2xl font-bold mb-4'>Login</h2>
+            {error && <p className=' text-red-600'>{error}</p> }
             <form action="" onSubmit={handelSubmit}>
             
                 <div className=' mb-4' >
                     <label htmlFor="email" className=' block text-gray-700'>Email</label>
                     <input type="email" placeholder='Enter Email' id='email' className=' w-full px-3 py-2 border rounded-md'
-                    onChange={(e)=> setEmail(e.target.value)}/>
+                    onChange={(e)=> setEmail(e.target.value)}
+                    required/>
                 </div>
                 <div className=' mb-4'>
                     <label htmlFor="password" className=' block text-gray-700'>Password</label>
                     <input type="password" placeholder='*******' id='password' className=' w-full px-3 py-2 border rounded-md'
-                    onChange={(e)=> setPassword(e.target.value)}/>
+                    onChange={(e)=> setPassword(e.target.value)}
+                    required/>
                 </div>
                 <div className=' mb-4 flex items-center justify-between'>
                     <label  className=' inline-flex items-center'>
@@ -37,7 +49,7 @@ const Login = () => {
                     <a href="" className=' text-teal-600'>Forgot Password</a>
                 </div>
                 <div className='mb-4'>
-                    <button type='submit' className=' w-full bg-teal-600 text-white py-2 rounded-md' >Login</button>
+                    <button type='submit' className=' w-full bg-teal-600 text-white py-2 rounded-md cursor-pointer' >Login</button>
                 </div>
                 
             </form>
