f0ec7a8ae50daf1d80314c68d74b46e723e7a541
AhmathNafees <ahamednafees15@gmail.com>
2025-06-17 13:38:37 +0530

    view customer jsx update

--- frontend/src/Components/customer/ViewCustomer.jsx
+++ frontend/src/Components/customer/ViewCustomer.jsx
@@ -44,56 +44,56 @@ const ViewCustomer = () => {
               <p className=' text-lg font-bold'>Passport Image</p>
               <img src={`http://localhost:3000/${customer.passportImage}`} alt="" className=' rounded-md w-72 border-2'  />
             </div>
-            <div>
-                <div className=' flex space-x-3 mb-5'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-y-1 gap-x-90'>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
                     <p className=' text-lg font-bold'>Name:</p>
                     <p className=' font-medium'>{customer.name}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
-                    <p className=' text-lg font-bold'>Mobile Number:</p>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
+                    <p className=' text-lg font-bold whitespace-nowrap'>Mobile Number:</p>
                     <p className=' font-medium'>{customer.pno}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
                     <p className=' text-lg font-bold'>Email:</p>
                     <p className=' font-medium'>{customer.email}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
-                    <p className=' text-lg font-bold'>Home Address:</p>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
+                    <p className=' text-lg font-bold whitespace-nowrap'>Home Address:</p>
                     <p className=' font-medium'>{customer.homeAdd}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
-                    <p className=' text-lg font-bold'>Purpose Descrption:</p>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5 '>
+                    <p className=' text-lg font-bold whitespace-nowrap'>Purpose Descrption:</p>
                     <p className=' font-medium'>{customer.desc}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
                     <p className=' text-lg font-bold'>NIC:</p>
                     <p className=' font-medium'>{customer.nic}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
-                    <p className=' text-lg font-bold'>Passport Number:</p>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
+                    <p className=' text-lg font-bold whitespace-nowrap'>Passport Number:</p>
                     <p className=' font-medium'>{customer.passport}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
-                    <p className=' text-lg font-bold'>Date of Birth:</p>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
+                    <p className=' text-lg font-bold whitespace-nowrap'>Date of Birth:</p>
                     <p className=' font-medium'>{new Date(customer.dob).toLocaleDateString()}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
                     <p className=' text-lg font-bold'>Gender:</p>
                     <p className=' font-medium'>{customer.gender}</p>
                 </div>
                 
-                <div className=' flex space-x-3 mb-5'>
-                    <p className=' text-lg font-bold'>Marital Status:</p>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
+                    <p className=' text-lg font-bold whitespace-nowrap'>Marital Status:</p>
                     <p className=' font-medium'>{customer.maritalStatus}</p>
                 </div>
                 
-                <div className=' flex space-x-3 mb-5'>
-                    <p className=' text-lg font-bold'>Branch Admin:</p>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
+                    <p className=' text-lg font-bold whitespace-nowrap'>Branch Admin:</p>
                     <p className=' font-medium'>{customer.userId.name}</p>
                 </div>
-                <div className=' flex space-x-3 mb-5'>
+                <div className=' flex flex-col md:flex-row md:items-center space-x-3 mb-5'>
                     <p className=' text-lg font-bold'>Branch:</p>
-                    <p className=' font-medium'>{customer.branchId.branch_name}</p>
+                    <p className=' font-medium whitespace-nowrap'>{customer.branchId.branch_name}</p>
                 </div>
             </div>
             
